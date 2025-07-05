8356addbdfc8077012471f16930d01f2607f17cd
Sknagma5233 <sknagma5233@gmail.com>
2025-07-05 17:05:05 +0530

    Home page created

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,6 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
